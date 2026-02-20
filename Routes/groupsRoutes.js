d8cf8e45d346a143d71900bafdfc91ea4ad73aab
jeyaprakshp4587 <jeyaprakashp022@gmail.com>
2026-02-20 23:41:26 +0530

    update

--- Routes/groupsRoutes.js
+++ Routes/groupsRoutes.js
@@ -7,15 +7,15 @@ import {
   remove,
   getSuggestionsGroups,
 } from "../Controllers/groupsController.js";
-import { verifyToken } from "../Middlewares/JWT.js";
+// import { } from "../Middlewares/JWT.js";
 
 const router = express.Router();
 
-router.get("/getAllGroups", verifyToken, getAll);
-router.get("/getParticularGroup/:id", verifyToken, getById);
-router.post("/createGroup", verifyToken, create);
-router.put("/updateGroup:id", verifyToken, update);
-router.delete("/deleteGroup/:id", verifyToken, remove);
-router.get("/getSuggestionGroups", verifyToken, getSuggestionsGroups);
+router.get("/getAllGroups", getAll);
+router.get("/getParticularGroup/:id", getById);
+router.post("/createGroup", create);
+router.put("/updateGroup:id", update);
+router.delete("/deleteGroup/:id", remove);
+router.get("/getSuggestionGroups", getSuggestionsGroups);
 
 export default router;
